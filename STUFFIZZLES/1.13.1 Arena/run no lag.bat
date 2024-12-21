3a742bdf45b2a4de6e900f8cb549ca267003376e
BuildTools <unconfigured@null.spigotmc.org>
2024-12-21 03:43:55 -0500

    stuffizzles 1.13.1 arena

---
+++ STUFFIZZLES/1.13.1 Arena/run no lag.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+"%ProgramFiles(x86)%\Java\jre1.8.0_181\bin\java.exe" -Xmx1512M -Xms1512M -jar spigot-1.13.1.jar
+PAUSE
---
+++ STUFFIZZLES/1.13.1 Arena/runn.bat
@@ -0,0 +1,1 @@
+java -Xmx1024M -Xms1024M -jar spigot-1.13.1.jar nogui
