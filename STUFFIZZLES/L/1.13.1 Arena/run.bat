77a5cc8c13de495533420090e7d8545241d14e6c
BuildTools <unconfigured@null.spigotmc.org>
2024-12-21 03:50:35 -0500

    some more folders in STUFFIZZLES

---
+++ STUFFIZZLES/L/1.13.1 Arena/run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+"%ProgramFiles(x86)%\Java\jre1.8.0_181\bin\java.exe" -Xmx1024M -Xms1024M -jar spigot-1.13.1.jar
+PAUSE
---
+++ STUFFIZZLES/hmm desktop/ZOINKS2/run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+"%ProgramFiles(x86)%\Java\jre1.8.0_181\bin\java.exe" -Xmx1024M -Xms1024M -jar server.jar
+PAUSE
